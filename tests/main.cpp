ac7834236cf5a7efc1567628bacd438911102cbe
MadNiko <for-niko@yandex.ru>
2016-12-09 01:06:59 +0300

    "игра" с тестом

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,24 +4,37 @@
 
 #include <logger/node.h>
 #include <logger/manager.h>
+#include <logger/function_visiter.h>
 #include <logger/def_target/ostream.h>
 
 
 int main()
 {
-    logger::manager logger_manager(new logger::def_target::smart_ostream_on_heap(std::cout));
+    logger::node log_main;
 
-    logger_manager.running_app();
+    {
+        logger::manager logger_manager(new logger::def_target::smart_ostream_on_heap(std::cout));
 
-    logger_manager.system("Hello, world!");
+        log_main = logger_manager.make_node("main");
 
-    logger_manager.make_node("main").info("This message not enabled!");
+        logger_manager.running_app();
 
-    logger_manager.set_log_level(logger::level::debug);
+        logger_manager.system("Hello, world!");
 
-    logger_manager.make_node("main").info("Work test application...");
+        log_main.info("This message not enabled!");
 
-    logger_manager.quit_app();
+        logger_manager.set_log_level(logger::level::developer);
+
+        {
+            const logger::function_visiter visiter(log_main.visiter(__FUNCTION__));
+
+            logger_manager.make_node("main").info("Work test application...");
+        }
+
+        logger_manager.quit_app();
+    }
+
+    log_main.sub_node("not_manager").fatal("exit to appication");
 
     return 0;
 }
