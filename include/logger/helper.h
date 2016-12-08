89f153162b103a9dc2216510d142465b01e8c578
MadNiko <for-niko@yandex.ru>
2016-12-09 01:05:48 +0300

    корректировка отображаемого заголовка в сообщениях

--- include/logger/helper.h
+++ include/logger/helper.h
@@ -11,10 +11,12 @@
 #define LOGGER_HELPER_H
 
 
+#include <string>
+#include <cstdio>
 #include <cwchar>
 #include <cstring>
-#include <string>
-
+#include <cstdarg>
+#include <type_traits>
 
 #include <logger/types.h>
 
@@ -28,13 +30,45 @@ template<> struct chart<char>
 {
     inline static const char* find(const char* str, char ch) { return std::strchr(str, ch); }
     inline static std::size_t length_str(const char* str) { return std::strlen(str); }
-    inline static std::string number_to_string(std::size_t number) { return std::to_string(number); }
+
+    template<class _Number, class = std::enable_if_t<std::is_integral<_Number>::value>>
+    inline static std::string num_to_dec(_Number number, std::size_t min_width)
+    {
+        if (min_width > 32) min_width = 32;
+        char buffer[33] = {'\0'};
+        std::sprintf(buffer, "%.*d", min_width, number);
+        return std::string(buffer);
+    }
+
+    template<class _Number, class = std::enable_if_t<std::is_unsigned<_Number>::value>>
+    inline static std::string num_to_hex(_Number number)
+    {
+        char buffer[2*sizeof(_Number)+1] = {'\0'};
+        std::sprintf(buffer, "%.*X", sizeof(buffer)-1, number);
+        return std::string(buffer);
+    }
 };
 template<> struct chart<wchar_t>
 {
     inline static const wchar_t* find(const wchar_t* str, wchar_t ch) { return std::wcschr(str, ch); }
     inline static std::size_t length_str(const wchar_t* str) { return std::wcslen(str); }
-    inline static std::wstring number_to_string(std::size_t number) { return std::to_wstring(number); }
+
+    template<class _Number, class = std::enable_if_t<std::is_integral<_Number>::value>>
+    inline static std::wstring num_to_dec(_Number number, std::size_t min_width)
+    {
+        if (min_width > 32) min_width = 32;
+        wchar_t buffer[33] = {L'\0'};
+        std::swprintf(buffer, L"%.*d", min_width, number);
+        return std::wstring(buffer);
+    }
+
+    template<class _Number, class = std::enable_if_t<std::is_unsigned<_Number>::value>>
+    inline static std::wstring num_to_hex(_Number number)
+    {
+        logger::chr buffer[2*sizeof(_Number)+1] = {L'\0'};
+        std::swprintf(buffer, sizeof(buffer), L"%.*X", sizeof(buffer)-1, number);
+        return std::wstring(buffer);
+    }
 };
 
 
